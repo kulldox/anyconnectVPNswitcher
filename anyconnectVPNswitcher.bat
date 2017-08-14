bb1379c04167136455c2f50285a7d7a5be69a758
KullDox <kulldox@gmail.com>
2017-08-14 11:29:49 -0700

    added a profile restore from a backup, because some anyconnect servers will overwrite your custom settings. also did some additional checks and handling of various situations, as well as work around CMD way of saving command output to variable

--- anyconnectVPNswitcher.bat
+++ anyconnectVPNswitcher.bat
@@ -18,6 +18,10 @@ set VPNNAME="Cisco AnyConnect Secure Mobility Agent"
 set PAUSETIME=5
 set APPPATH="%ProgramFiles(x86)%\Cisco\Cisco AnyConnect Secure Mobility Client\"
 set APPVIPPATH="%ProgramFiles(x86)%\Symantec\VIP Access Client\"
+set VPNPROFILEBACKUP="C:\bkp\anyconnect\asOfAug72017\AnyConnect-General-Client-mct-custom-Profile.xml"
+set VPNPROFILE="%ProgramData%\Cisco\Cisco AnyConnect Secure Mobility Client\Profile\AnyConnect-General-Client-Profile.xml"
+set CHECKPROFILE="yes"
+setlocal EnableDelayedExpansion
 
 if "%1" == "VPNServer1" (
 	rem example of a generic server, that does not have a banner. Banner, is a little popup kind of window that is opened once you connect to the VPN, and it requires you to accept some rules.
@@ -50,51 +54,69 @@ rem *** END CONFIGURATION ***
 	goto exitScript
 )
 
-@ECHO Check if %PROFILENAME% exists, if no, exit
+rem due to problems with paths having spaces failing in the for loops, just CD into the dir
+cd %APPPATH%
 
-rem just a hack to save the command output into a variable
-set OUTPUT=""
-for /f %%i in ('vpncli.exe hosts ^| find %PROFILENAME%') do set OUTPUT="%%i"
-if %OUTPUT% == "" (
-	@ECHO /!\ The profile %PROFILENAME% does NOT exist. Exit.
-	goto exitScript
+if %CHECKPROFILE% == "yes" (
+	rem some VPNs will overwrite the profile
+	@ECHO INFO: Restore the custom profile from %VPNPROFILEBACKUP%
+	xcopy /y %VPNPROFILEBACKUP% %VPNPROFILE%
+	@ECHO.
 )
-cd
 
-@ECHO Disconnect the vpncli.exe application
+if %PROFILENAME% NEQ "disconnect" (
+	@ECHO INFO: Check if %PROFILENAME% exists, if no, exit
+	rem just a hack to save the command output into a variable
+	set OUTPUT=""
+	vpncli.exe hosts
+	for /f "delims=" %%i in ('vpncli.exe hosts ^| find %PROFILENAME%') do ( set "OUTPUT=%%~i" )
+	@ECHO "value found !OUTPUT!"
+	if "!OUTPUT!" == "" (
+		@ECHO "/!\ ERROR: The profile %PROFILENAME% does NOT exist. Exit."
+		vpncli.exe hosts
+		goto exitScript
+	)
+)
+@ECHO INFO: Disconnect the vpncli.exe application
 %APPPATH%vpncli.exe disconnect
 @ECHO Wait %PAUSETIME% seconds
 TIMEOUT /t %PAUSETIME% /NOBREAK
 
 if %PROFILENAME% == "disconnect" (
-	@ECHO Just disconnect the VPN client
+	@ECHO INFO: Just disconnect the VPN client
 	goto exitScript
 )
 
-@ECHO Check if %VPNNAME% Service is up and running, if yes, stop it
+@ECHO INFO: Check if %VPNNAME% Service is up and running, if yes, stop it
 rem just a hack to save the command output into a variable
 set OUTPUT=""
-for /f %%i in ('NET START ^| find %VPNNAME%') do set OUTPUT=%%i
-if NOT %OUTPUT% == "" (
-	@ECHO Stopping %VPNNAME% Service, please wait for %PAUSETIME% seconds
+for /f  "delims=" %%i in ('NET START ^| find %VPNNAME%') do set "OUTPUT=%%~i"
+if NOT "!OUTPUT!" == "" (
+	@ECHO INFO: Stopping %VPNNAME% Service, please wait for %PAUSETIME% seconds
 	rem runas /user:Administrator NET STOP %VPNNAME%
 	NET STOP %VPNNAME%
 	TIMEOUT /t %PAUSETIME% /NOBREAK
 )
 
-@ECHO Check if %VPNNAME% Service is up and running
+rem loop until the service is up and running
+:checkVPNServiceup
+@ECHO INFO: Check if %VPNNAME% Service is up and running
 rem just a hack to save the command output into a variable
 set OUTPUT=""
-for /f %%i in ('NET START ^| find %VPNNAME%') do set OUTPUT=%%i
-if %OUTPUT% == "" (
-	@ECHO Starting %VPNNAME% Service
+for /f  "delims=" %%i in ('NET START ^| find %VPNNAME%') do set "OUTPUT=%%~i"
+@ECHO "value of OUTPUT: !OUTPUT!"
+if "!OUTPUT!" == "" (
+	@ECHO INFO: %VPNNAME% Service is not up.
+	@ECHO INFO: Starting %VPNNAME% Service
 	NET START %VPNNAME%
+	TIMEOUT /t %PAUSETIME% /NOBREAK
+	goto checkVPNServiceup
 )
 
 rem if this is not production, start the vpncli.exe with profile, user and pass
-if NOT "%PRODUCTION%" == "production" (
-	@ECHO Start the vpncli.exe application
-	echo connect %PROFILENAME%^& echo %PROFILEUSER%^&echo %PROFILEPASS%^&echo y
+if NOT "%~PRODUCTION%" == "production" (
+	@ECHO INFO: Start the vpncli.exe application
+	@ECHO INFO: connect %PROFILENAME%^& echo %PROFILEUSER%^&echo %PROFILEPASS%^&echo y
 	if %PROFILEBANNER% == "y" (
 		(echo connect %PROFILENAME%^& echo %PROFILEUSER%^&echo %PROFILEPASS%^&echo y^&rem.) | %APPPATH%vpncli.exe -s
 	)
@@ -105,18 +127,20 @@ if NOT "%PRODUCTION%" == "production" (
 )
 
 rem if this is production which is using multiple step authentication, so use GUIs, thus just start the 'Symantec VIP access' and vpnui.exe
-if "%PRODUCTION%" == "production" (
-	@ECHO This is production, start Symantec VIP access
+if "%~PRODUCTION%" == "production" (
+	@ECHO INFO: This is production, start Symantec VIP access
 	start "" /D%APPVIPPATH% VIPUIManager.exe
-	@ECHO This is production, just start the GUI version vpnui.exe
+	@ECHO INFO: This is production, just start the GUI version vpnui.exe
 	start "" /D%APPPATH% vpnui.exe
 	TIMEOUT /t %PAUSETIME% /NOBREAK
 )
 
+endlocal
+
 :exitScript
-@ECHO Wait %PAUSETIME% seconds
+@ECHO INFO: Wait %PAUSETIME% seconds
 TIMEOUT /t %PAUSETIME% /NOBREAK
 
-@ECHO "Done. Have FUN :)"
-PAUSE
+@ECHO "INFO: Done. Have FUN :)"
+rem PAUSE
 EXIT
